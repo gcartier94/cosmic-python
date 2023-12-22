3730b87da0a426d6a90e42683229273b6bfa4450
Guilherme Cartier <gcartier@kyndryl.com>
2023-12-22 19:35:27 -0300

    updates to testing routines
    Signed-off-by: Guilherme Cartier <gcartier@kyndryl.com>

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	pytest --ignore=results/
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,4 +1,5 @@
 from repository.repository import AbstractRepository
+from model.model import OrderLine
 from model.model import Batch
 
 
@@ -26,13 +27,40 @@ def insert_order_line(session):
     return order_line_id
 
 def insert_batch(session, batch_id):
-    pass
+    session.execute(
+        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+        ' VALUES (:batch_id, "GENERIC-SOFA", 100, null)',
+        dict(batch_id=batch_id),
+    )
+    [[batch_id]] = session.execute(
+        'SELECT id FROM batches WHERE reference=:batch_id AND sku="GENERIC-SOFA"',
+        dict(batch_id=batch_id),
+    )
+    return batch_id
 
-def insert_allocation(session):
-    pass
+
+def insert_allocation(session, orderline_id, batch_id):
+    session.execute(
+        "INSERT INTO allocations (orderline_id, batch_id)"
+        " VALUES (:orderline_id, :batch_id)",
+        dict(orderline_id=orderline_id, batch_id=batch_id),
+    )
+
 
 def test_repository_can_retrieve_a_batch_with_allocations(session):
     orderline_id = insert_order_line(session)
-    batch1_id = insert_batch(session, "BATCH001")
-    insert_batch(session, "BATCH002")
-    pass 
+    batch1_id = insert_batch(session, "batch1")
+    insert_batch(session, "batch2")
+    insert_allocation(session, orderline_id, batch1_id)
+
+    repo = SqlAlchemyRepository(session)
+    retrieved = repo.get("batch1")
+
+    expected = Batch("batch1", "GENERIC-SOFA", 100, eta=None)
+    assert retrieved == expected  # Batch.__eq__ only compares reference
+    assert retrieved.sku == expected.sku
+    assert retrieved._purchased_quantity == expected._purchased_quantity
+    assert retrieved._allocations == {
+        OrderLine("order1", "GENERIC-SOFA", 12),
+    }
+
