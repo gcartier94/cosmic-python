6191d7e6f066d52fbcfa5ad951c5fbbbc288d026
Guilherme Cartier <gcartier@kyndryl.com>
2023-12-22 19:43:32 -0300

    completed chapter2
    Signed-off-by: Guilherme Cartier <gcartier@kyndryl.com>

--- repository/repository.py
+++ repository/repository.py
@@ -11,4 +11,16 @@ class AbstractRepository(ABC):
     def get(self, reference) -> Batch:
         raise NotImplementedError
 
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch: Batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
 
